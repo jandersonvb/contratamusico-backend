d6228954450eff26a2e5c79b141f5e6d1a93b8db
Janderson Vilas Boas <jandersonvb.dev@gmail.com>
2025-06-08 23:06:41 -0300

    feat: configure @nestjs/config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // Torna o ConfigModule global, acessível em todo o aplicativo
+      envFilePath: '.env', // Incdica o caminho do seu arquivo .env
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
