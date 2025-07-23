1ee7f313f931ec63441536418106deb671eb26dc
Janderson Vilas Boas <jandersonvb.dev@gmail.com>
2025-07-22 23:14:49 -0300

    feat: change in main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,33 +2,34 @@
 import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config'; // Importe ConfigService
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // Habilita a validação global de DTOs
   app.useGlobalPipes(new ValidationPipe({
-    whitelist: true, // Remove propriedades que não estão no DTO
-    forbidNonWhitelisted: true, // Lança erro se houver propriedades não permitidas
-    transform: true, // Transforma payloads de entrada em instâncias DTO
-    transformOptions: {
-      enableImplicitConversion: true, // Converte tipos automaticamente (ex: '123' para 123)
-    },
+    whitelist: true,
+    forbidNonWhitelisted: true,
+    transform: true,
+    transformOptions: { enableImplicitConversion: true, },
   }));
 
+  const configService = app.get(ConfigService);
+  // Pega a URL do frontend do .env (NEXT_PUBLIC_FRONTEND_BASE_URL)
+  const frontendUrl = configService.get<string>('frontendUrl');
 
-  // Habilita CORS para permitir requisições do frontend
   app.enableCors({
-    origin: '*', // Permite todas as origens. Em produção, restrinja! ex: ['https://www.contratamusico.com.br']
+    // Use a URL do frontend para produção e localhost para desenvolvimento
+    origin: [
+      'http://localhost:3001', // Para seu desenvolvimento local
+      'https://www.contratamusico.com.br', // <--- ADICIONE ESTA LINHA (DOMÍNIO DE PRODUÇÃO DO SEU FRONTEND)
+      frontendUrl // Se você definir NEXT_PUBLIC_FRONTEND_BASE_URL no Railway para o mesmo domínio
+    ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    credentials: true, // Se você for usar cookies/sessões
+    credentials: true,
   });
 
-  // Pega o serviço de configuração para acessar variáveis de ambiente
-  const configService = app.get(ConfigService);
-  const port = configService.get<number>('port') ?? 3000; // 'port' definido em configuration.ts, fallback para 3000
-
+  const port = configService.get<number>('port') ?? 3000;
   await app.listen(port);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
