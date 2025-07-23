81581b4e08f7c0fd089ca53c2ceaacb8231fe324
Janderson Vilas Boas <jandersonvb.dev@gmail.com>
2025-07-23 00:21:17 -0300

    feat: add console.log in configuration

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,26 +1,30 @@
-export default () => ({
-  port: parseInt(process.env.PORT || '3000', 10),
-  database: {
-    type: process.env.DATABASE_TYPE || 'postgres',
-    host: process.env.DATABASE_HOST,
-    port: parseInt(process.env.DATABASE_PORT || '5432', 10),
-    username: process.env.DATABASE_USERNAME,
-    password: process.env.DATABASE_PASSWORD,
-    name: process.env.DATABASE_NAME,
-  },
-  jwt: {
-    secret: process.env.JWT_SECRET || 'umSuperSegredoMuitoComplexoParaDesenvolvimento', // Use uma variável de ambiente real!
-    expiresIn: process.env.JWT_EXPIRES_IN || '1h',
-  },
-  google: {
-    clientId: process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackUrl: process.env.GOOGLE_CALLBACK_URL,
-  },
-  facebook: {
-    clientId: process.env.FACEBOOK_CLIENT_ID,
-    clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
-    callbackUrl: process.env.FACEBOOK_CALLBACK_URL,
-  },
-  frontendUrl: process.env.NEXT_PUBLIC_FRONTEND_BASE_URL,
-});
+export default () => {
+  console.log(`Database configuration: ${process.env.DATABASE_TYPE}://${process.env.DATABASE_USERNAME}:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_HOST}:${process.env.DATABASE_PORT}/${process.env.DATABASE_NAME}`);
+
+  return {
+    port: parseInt(process.env.PORT || '3000', 10),
+    database: {
+      type: process.env.DATABASE_TYPE || 'postgres',
+      host: process.env.DATABASE_HOST,
+      port: parseInt(process.env.DATABASE_PORT || '5432', 10),
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      name: process.env.DATABASE_NAME,
+    },
+    jwt: {
+      secret: process.env.JWT_SECRET || 'umSuperSegredoMuitoComplexoParaDesenvolvimento', // Use uma variável de ambiente real!
+      expiresIn: process.env.JWT_EXPIRES_IN || '1h',
+    },
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackUrl: process.env.GOOGLE_CALLBACK_URL,
+    },
+    facebook: {
+      clientId: process.env.FACEBOOK_CLIENT_ID,
+      clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+      callbackUrl: process.env.FACEBOOK_CALLBACK_URL,
+    },
+    frontendUrl: process.env.NEXT_PUBLIC_FRONTEND_BASE_URL,
+  };
+};
