8fc07a14bed1b8b40d0da448309922eaf24fd411
Janderson Vilas Boas <jandersonvb.dev@gmail.com>
2025-07-23 00:03:31 -0300

    feat: changes to deploy

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,6 +1,7 @@
 export default () => ({
   port: parseInt(process.env.PORT || '3000', 10),
   database: {
+    type: process.env.DATABASE_TYPE || 'postgres',
     host: process.env.DATABASE_HOST,
     port: parseInt(process.env.DATABASE_PORT || '5432', 10),
     username: process.env.DATABASE_USERNAME,
