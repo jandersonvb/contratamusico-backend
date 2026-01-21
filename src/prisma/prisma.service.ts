2d39161dfcfaba69c51e9d062929f1f0e11c34bb
Janderson Vilas Boas <jandersonvb.dev@gmail.com>
2026-01-20 23:35:34 -0300

    fix: use adapter

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -5,10 +5,15 @@ import {
   type INestApplication,
 } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
+import { PrismaMariaDb } from '@prisma/adapter-mariadb';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
-
+  constructor() {
+    const adapter = new PrismaMariaDb(process.env.DATABASE_URL!);
+    
+    super({ adapter });
+  }
 
   async onModuleInit() { // Conecta ao banco de dados quando o módulo é inicializado
     await this.$connect();
